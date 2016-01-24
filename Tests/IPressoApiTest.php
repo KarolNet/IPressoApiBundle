746daa71104bf21e80987f42155e340d5629eb08
Karol Grochowalski <karol.grochowalski@gmail.com>
2016-01-24 15:09:53 +0100

    test refactor

--- Tests/IPressoApiTest.php
+++ Tests/IPressoApiTest.php
@@ -60,55 +60,59 @@ class IPressoApiTest extends \PHPUnit_Framework_TestCase
 
     private function thenIcanAuthenticate()
     {
-        $iPressoApi = new IPressoApi('foobar', 'login', 'pass', 'http://localhost', $this->guzzleClient);
+        $iPressoApi = $this->getIPressoApi();
         $token = $iPressoApi->accessToken();
         $this->assertEquals($token, 'cfa9eb3af4f7f8b7c39a3213213431fdsff8e571');
     }
 
     private function thenIcantAuthenticate()
     {
-        $iPressoApi = new IPressoApi('foobar', 'login', 'pass', 'http://localhost', $this->guzzleClient);
+        $iPressoApi = $this->getIPressoApi();
         $iPressoApi->accessToken();
     }
 
     private function thereIsSuccessFullIPressoApiResponse()
     {
-        $response = $this->mockResponse(200, '{"code":200,"data":"cfa9eb3af4f7f8b7c39a3213213431fdsff8e571","message":"OK"}');
-        $handler = HandlerStack::create($response);
-        $this->guzzleClient = new Client(['handler' => $handler]);
+        $this->setGuzzleClientResponse(200, '{"code":200,"data":"cfa9eb3af4f7f8b7c39a3213213431fdsff8e571","message":"OK"}');
     }
 
     private function thereIsClientExistResponse()
     {
-        $response = $this->mockResponse(200, '{"code":200,"data":{"contact":["26413"]},"message":"OK"}');
-        $handler = HandlerStack::create($response);
-        $this->guzzleClient = new Client(['handler' => $handler]);
+        $this->setGuzzleClientResponse(200, '{"code":200,"data":{"contact":["26413"]},"message":"OK"}');
     }
 
     private function thereIsClientNonExistResponse()
     {
-        $response = $this->mockResponse(404, '{"code":404,"data":{"contact":[]},"message":"Not Found","errorCode":3}');
-        $handler = HandlerStack::create($response);
-        $this->guzzleClient = new Client(['handler' => $handler]);
+        $this->setGuzzleClientResponse(404, '{"code":404,"data":{"contact":[]},"message":"Not Found","errorCode":3}');
     }
 
     private function thenClientNotFound()
     {
-        $iPressoApi = new IPressoApi('foobar', 'login', 'pass', 'http://localhost', $this->guzzleClient);
+        $iPressoApi = $this->getIPressoApi();
         $this->assertNull($iPressoApi->findContact('not-existing@contact.pl', 'ssss'));
     }
 
     private function thenFundedClients()
     {
-        $iPressoApi = new IPressoApi('foobar', 'login', 'pass', 'http://localhost', $this->guzzleClient);
+        $iPressoApi = $this->getIPressoApi();
         $iPressoApi->findContact('test@user.pl', 'aaaaaa');
     }
 
     private function thereIsBadIPressoApiResponse()
     {
-        $response = $this->mockResponse(401, '{"code":401,"message":"Unauthorized: Wrong customer token"}');
+        $this->setGuzzleClientResponse(401, '{"code":401,"message":"Unauthorized: Wrong customer token"}');
+    }
+
+    private function setGuzzleClientResponse($httpStatus, $jsonString)
+    {
+        $response = $this->mockResponse($httpStatus, $jsonString);
         $handler = HandlerStack::create($response);
         $this->guzzleClient = new Client(['handler' => $handler]);
+    }
+
+    private function getIPressoApi()
+    {
+        return new IPressoApi('foobar', 'login', 'pass', 'http://localhost', $this->guzzleClient);
     }
 
     private function mockResponse($status, $body = null)
